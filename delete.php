5c543704f6bfa30ac8e7fd857743287bf91c0c72
owguh <guh.oliveira.lopes@gmail.com>
2024-10-11 06:23:08 -0300

    new commit

--- delete.php
+++ delete.php
@@ -1,1 +1,20 @@
-deletado
+deletado<?php
+    include_once('crud.php');
+
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+
+        // Executar a exclusão no banco de dados
+        $sqlDelete = "DELETE FROM formulario WHERE id = '$id'";
+
+        if(mysqli_query($conexao, $sqlDelete)){
+            echo "Registro excluído com sucesso!";
+        } else {
+            echo "Erro ao excluir o registro: " . mysqli_error($conexao);
+        }
+
+        // Redirecionar de volta para a página de listagem após a exclusão
+        header("Location: read.php");
+        exit();
+    }
+?>
